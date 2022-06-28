886d04221651b80ff446f10d0a53c065de93bfec
eno-chang <89822056+eno-chang@users.noreply.github.com>
2022-06-29 00:34:34 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,18 +2,26 @@ for (var i = 1; i <= 20; i++) //1から20までを繰り返す
 {
   if (i % 3 == 0 && i % 5 ==0) //3と5の最小公倍数15で割り切れる数字の時
   {
-    document.write('FizzBuzz','<br>');
+    let moji = document.createElement('p');
+    moji.innerHTML = 'FizzBuzz','<br>';
+    document.body.appendChild(moji);
   } 
   else if (i % 3 == 0) //3で割り切れる数字の時
   {
-    document.write('Fizz','<br>');
+    let moji = document.createElement('p');
+    moji.innerHTML = 'Fizz','<br>';
+    document.body.appendChild(moji);
   }
   else if (i % 5 == 0) //5で割り切れる数字の時
   {
-    document.write('Buzz','<br>');
+    let moji = document.createElement('p');
+    moji.innerHTML = 'Buzz','<br>';
+    document.body.appendChild(moji);
   } 
   else //そのほか
   {
-    document.write(i,'<br>');
+    let moji = document.createElement('p');
+    moji.innerHTML = i,'<br>';
+    document.body.appendChild(moji);
   }
 }
