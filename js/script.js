7d0e36c3b78b18274e987eabf61d0a052f49912b
eno-chang <89822056+eno-chang@users.noreply.github.com>
2022-06-28 23:33:33 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,18 +2,18 @@ for (var i = 1; i <= 20; i++) //1から20までを繰り返す
 {
   if (i % 3 == 0 && i % 5 ==0) //3と5の最小公倍数15で割り切れる数字の時
   {
-    console.log('FizzBuzz');
+    document.write('FizzBuzz');
   } 
   else if (i % 3 == 0) //3で割り切れる数字の時
   {
-    console.log('Fizz');
+    document.write('Fizz');
   }
   else if (i % 5 == 0) //5で割り切れる数字の時
   {
-    console.log('Buzz');
+    document.write('Buzz');
   } 
   else //そのほか
   {
-    console.log(i);
+    document.write(i);
   }
 }
